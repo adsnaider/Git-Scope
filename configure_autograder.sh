4e851d6c6826d58ccb51698d5f67f59d59358021
Adam Snaider <adsnaider@gmail.com>
2020-03-07 11:24:40 -0800

    Created script to automatically setup and create autograder.zip.

---
+++ configure_autograder.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 Github Address" >&2
+  echo "Example: $0 git@github.com/foo/bar.git" >&2
+  exit 1
+fi
+
+wget https://github.com/adsnaider/gradescope-integration/archive/master.zip > /dev/null 2>&1
+unzip master.zip -d autograder > /dev/null
+
+rm master.zip
+
+cd autograder/gradescope-integration-master/
+
+rm -f deploy_key deploy_key.pub
+ssh-keygen -f deploy_key -q -N ""
+
+echo "============================= Deploy Key (pub) ==================================="
+cat deploy_key.pub
+echo "=========================== End Deploy Key (pub) ================================="
+echo "Please copy the above key to the deploy keys section in your github repo."
+
+echo "$1" > repo
+echo "Setting repo address as \"$1\""
+
+zip ../../autograder.zip ./* > /dev/null
+cd ../../
+rm -rf autograder/
