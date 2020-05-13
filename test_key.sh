eee936d87eb000ae0e7d0651db71e0b3c8fdb8bf
4onen <mattjdupree@gmail.com>
2020-05-13 09:10:27 -0700

    Prevent fallback to regular identities when local deploy_key fails.

--- test_key.sh
+++ test_key.sh
@@ -6,11 +6,15 @@ if ! [ -f autograder/repo ]; then
   exit 1
 fi
 
-if GIT_SSH_COMMAND="ssh -i autograder/deploy_key -F /dev/null" git ls-remote -q --refs "$(cat autograder/repo)" HEAD; then
+sed "s;/root/.ssh/deploy_key;$(realpath autograder/deploy_key);" autograder/ssh_config > local_ssh_config
+
+if GIT_SSH_COMMAND="ssh -F local_ssh_config" git ls-remote -q --refs "$(cat autograder/repo)" HEAD; then
   echo 'The repo responded successfully. Good to go!'
+  rm local_ssh_config
   exit 0
 else
   echo 'There was a problem connecting. Is the deploy key setup on GitHub?'
   echo 'To re-test, run `./test.sh`'
+  rm local_ssh_config
   exit 1
 fi
